3f723681fa01e80cdc9859cf04fb1cca43692ba7
FACUSS <1563059699@qq.com>
2017-12-27 21:30:30 +0800

    li

--- KotlinMovie/app/src/main/java/baiye/ali/com/kotlinmovie/me/Fragment4.kt
+++ KotlinMovie/app/src/main/java/baiye/ali/com/kotlinmovie/me/Fragment4.kt
@@ -1,19 +1,31 @@
 package baiye.ali.com.kotlinmovie.me
 
+import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import baiye.ali.com.kotlinmovie.BaseFragment
 import baiye.ali.com.kotlinmovie.R
 import baiye.ali.com.kotlinmovie.me.activity.AdviseActivity
 import baiye.ali.com.kotlinmovie.me.activity.SavaActivity
-import baiye.ali.com.kotlinmovie.me.activity.UpdownActivity
 import baiye.ali.com.kotlinmovie.me.activity.WatchsActivity
 import kotlinx.android.synthetic.main.fragment4.*
+import java.io.File
 
 /**
  * Created by 31962 on 2017/12/27.
  */
 class Fragment4 : BaseFragment<MePresenter>(){
+
+    private var mBitmap: Bitmap? = null
+    protected val CHOOSE_PICTURE = 0
+    protected val TAKE_PICTURE = 1
+    private val CROP_SMALL_PICTURE = 2
+    protected lateinit var tempUri: Uri
 
     override fun getPresenter(): MePresenter {
         var mepresenter:MePresenter = MePresenter()
@@ -31,9 +43,10 @@ class Fragment4 : BaseFragment<MePresenter>(){
         tv_tx.setOnClickListener(object :View.OnClickListener{
             override fun onClick(v: View?) {
 
-                var intent = Intent(activity, UpdownActivity::class.java)
-                startActivity(intent)
+                /*var intent = Intent(activity, UpdownActivity::class.java)
+                startActivity(intent)*/
 
+                showChoosePicDialog()
 
             }
         })
@@ -74,11 +87,81 @@ class Fragment4 : BaseFragment<MePresenter>(){
     }
 
 
-
     override fun getlayoutId(): Int {
-
 
         return R.layout.fragment4
     }
+
+    /**
+     * 显示修改图片的对话框
+     */
+    private fun showChoosePicDialog() {
+
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("添加图片")
+        val items = arrayOf("选择本地照片", "拍照")
+        builder.setNegativeButton("取消", null)
+
+        builder.setItems(items) { dialog, which ->
+            when (which) {
+            // 选择本地照片
+                CHOOSE_PICTURE -> {
+                    val openAlbumIntent = Intent(
+                            Intent.ACTION_GET_CONTENT)
+                    openAlbumIntent.type = "image/*"
+                    //用startActivityForResult方法，重写onActivityResult()方法，拿到图片做裁剪操作
+                    startActivityForResult(openAlbumIntent, CHOOSE_PICTURE)
+                }
+             // 拍照
+                TAKE_PICTURE-> {
+                    val openCameraIntent = Intent(
+                            MediaStore.ACTION_IMAGE_CAPTURE)
+                    tempUri = Uri.fromFile(File(Environment
+                            .getExternalStorageDirectory(), "temp_image.jpg"))
+                    // 将拍照所得的相片保存到SD卡根目录
+                    openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri)
+                    startActivityForResult(openCameraIntent, TAKE_PICTURE)
+                }
+            }
+        }
+        builder.show()
+
+    }
+
+       /**
+         * 裁剪图片方法实现
+         */
+    protected fun cutImage(uri: Uri?) {
+            if (uri == null) {
+                Log.i("alanjet", "The uri is not exist.")
+            }
+            tempUri = uri!!
+            val intent = Intent("com.android.camera.action.CROP")
+            //com.android.camera.action.CROP这个action是用来裁剪图片用的
+            intent.setDataAndType(uri, "image/*")
+            // 设置裁剪
+            intent.putExtra("crop", "true")
+            // aspectX aspectY 是宽高的比例
+            intent.putExtra("aspectX", 1)
+            intent.putExtra("aspectY", 1)
+            // outputX outputY 是裁剪图片宽高
+            intent.putExtra("outputX", 150)
+            intent.putExtra("outputY", 150)
+            intent.putExtra("return-data", true)
+            startActivityForResult(intent, CROP_SMALL_PICTURE)
+        }
+
+        /**
+         * 保存裁剪之后的图片数据
+         */
+        protected fun setImageToView(data: Intent) {
+            val extras = data.extras
+            if (extras != null) {
+                mBitmap = extras.getParcelable<Bitmap>("data")
+                //利用工具将头像切成圆形
+                tv_tx.setImageBitmap(mBitmap)//显示图片
+
+            }
+        }
 
 }
--- KotlinMovie/app/src/main/java/baiye/ali/com/kotlinmovie/me/activity/AdviseActivity.kt
+++ KotlinMovie/app/src/main/java/baiye/ali/com/kotlinmovie/me/activity/AdviseActivity.kt
@@ -2,7 +2,9 @@ package baiye.ali.com.kotlinmovie.me.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import baiye.ali.com.kotlinmovie.R
+import kotlinx.android.synthetic.main.activity_advise.*
 
 /*
 * 意见反馈
@@ -13,5 +15,17 @@ class AdviseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_advise)
+
+        //返回上一层
+        fan.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+               finish()
+            }
+
+
+        })
     }
+
+
 }
