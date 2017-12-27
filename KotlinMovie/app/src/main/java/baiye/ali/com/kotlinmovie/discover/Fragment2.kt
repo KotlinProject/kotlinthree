761aef4d948f131194f5ae7ee95d8a682a26ed91
DSeptember <196016504@qq.com>
2017-12-27 20:18:49 +0800

    du

--- KotlinMovie/app/src/main/java/baiye/ali/com/kotlinmovie/discover/Fragment2.kt
+++ KotlinMovie/app/src/main/java/baiye/ali/com/kotlinmovie/discover/Fragment2.kt
@@ -4,16 +4,21 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import baiye.ali.com.kotlinmovie.BaseFragment
 import baiye.ali.com.kotlinmovie.R
+import baiye.ali.com.kotlinmovie.discover.bean.DiscoverBean
 import baiye.ali.com.kotlinmovie.discover.presenter.DiscoverPresenter
+import baiye.ali.com.kotlinmovie.discover.view.DiscoverView
 
 /**
  * Created by 31962 on 2017/12/27.
  */
-class Fragment2 : BaseFragment<DiscoverPresenter>() {
+class Fragment2 : BaseFragment<DiscoverPresenter>(),DiscoverView{
+    override fun DiscoverV(discoverBean: DiscoverBean) {
+
+    }
 
 
     override fun getPresenter(): DiscoverPresenter {
-        var discoverpresenter : DiscoverPresenter = DiscoverPresenter()
+        var discoverpresenter : DiscoverPresenter = DiscoverPresenter(this)
         return discoverpresenter
 
     }
