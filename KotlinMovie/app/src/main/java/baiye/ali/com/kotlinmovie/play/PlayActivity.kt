b0bc06c92b86fc6c6478b1a4f1db0fcc6668a0d9
BaiYe <bai16868@aliyun.com>
2017-12-28 11:57:10 +0800

    bai

---
+++ KotlinMovie/app/src/main/java/baiye/ali/com/kotlinmovie/play/PlayActivity.kt
@@ -0,0 +1,14 @@
+package baiye.ali.com.kotlinmovie.play
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import baiye.ali.com.kotlinmovie.R
+
+class PlayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
